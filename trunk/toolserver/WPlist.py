331af98b3e23a02bd883eb3ff62f4f89ebcc7e80
legoktm.wikipedia <legoktm.wikipedia@03ba9718-dac9-11dd-86d7-074df07e0730>
2009-01-28 06:41:09 +0000

    update WPlist.py
    git-svn-id: http://legobot.googlecode.com/svn@69 03ba9718-dac9-11dd-86d7-074df07e0730

--- trunk/toolserver/WPlist.py
+++ trunk/toolserver/WPlist.py
@@ -59,7 +59,8 @@ def retpages(cat):
 	for article in res:
 		try:
 			if article['ns'] != (14 or '14'):
-				wikitext += '*[[%s]]\n' %str(article['title']).encode("utf-8")
+				artname = unicode(article['title'])
+				wikitext += '*[[%s]]\n' %artname
 		except KeyError:
 			return ''
 	return wikitext
