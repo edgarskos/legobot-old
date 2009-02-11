957dc9516aaa85f40f8ad90f71ff753fd6208d93
legoktm.wikipedia <legoktm.wikipedia@03ba9718-dac9-11dd-86d7-074df07e0730>
2009-02-11 03:13:13 +0000

    add pagegenerators script
    git-svn-id: http://legobot.googlecode.com/svn@100 03ba9718-dac9-11dd-86d7-074df07e0730

---
+++ trunk/pywiki/pagegen.py
@@ -0,0 +1,50 @@
+#!usr/bin/python
+#
+# (C) Legoktm 2009
+# Released under the MIT License
+#
+# All pages should be in the wiki.Page() format
+
+import wiki
+#set up api
+API = wiki.API()
+
+"""Gets all articles in a certain category and returns a list"""
+def category(page, excludens = False):
+	if not page.isCategory():
+		raise wiki.NotCategory(page.title())
+	params = {
+		'action':'query',
+		'list':'categorymembers',
+		'cmtitle':page.title(),
+		'cmlimit':'500',
+	}
+	result = API.query(params)
+	#format the list
+	list = []
+	res = result['query']['categorymembers']
+	for page in res:
+		if excludens:
+			if page['ns'] != int(excludens):
+				list.append(unicode(wiki.Page(page['title'])))
+		else:
+			list.append(unicode(wiki.Page(page['title'])))
+	return list
+
+def prefixindex(page):
+	ns = page.namespace()
+	prefix = page.titlewonamespace() + '/'
+	params = {
+		'action':'query',
+		'list':'alllinks',
+		'alprefix':prefix,
+		'alnamespace':ns,
+		'allimit':'500',
+	}
+	res = API.query(params)['query']['alllinks']
+	list = []
+	for page in res:
+		wikipage = wiki.Page(page['title'])
+		if not (wikipage in list):
+			list.append(wikipage)
+	return list
