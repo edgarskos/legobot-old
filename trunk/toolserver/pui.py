b7be60eff8e0c82741dda674824aa748e33e7fdd
legoktm.wikipedia <legoktm.wikipedia@03ba9718-dac9-11dd-86d7-074df07e0730>
2009-02-28 04:31:19 +0000

    convert pui.py to pywiki framework
    git-svn-id: http://legobot.googlecode.com/svn@130 03ba9718-dac9-11dd-86d7-074df07e0730

--- trunk/toolserver/pui.py
+++ trunk/toolserver/pui.py
@@ -5,14 +5,12 @@
 # (C) Legoktm 2008-2009, MIT License
 # 
  
-import re, sys, os
-sys.path.append(os.environ['HOME'] + '/pyenwiki')
-import wikipedia
-site = wikipedia.getSite()
-page = wikipedia.Page(site, 'Wikipedia:Possibly unfree images')
+import re
+sys.path.append(os.environ['HOME'] + '/pywiki')
+import wiki
+page = wiki.Page('Wikipedia:Possibly unfree images')
 wikitext = state0 = page.get()
 wikitext = re.compile(r'\n==New listings==', re.IGNORECASE).sub(r'\n*[[/{{subst:#time:Y F j|-14 days}}]]\n==New listings==', wikitext)
 EditMsg = 'Adding new day to holding cell'
-wikipedia.showDiff(state0, wikitext)
-wikipedia.setAction(EditMsg)
-page.put(wikitext)
+wiki.showDiff(state0, wikitext)
+page.put(wikitext,EditMsg)
