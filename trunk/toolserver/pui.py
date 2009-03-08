acd31f2890925794d289e0ddd5dac9b7cd77dd45
legoktm.wikipedia <legoktm.wikipedia@03ba9718-dac9-11dd-86d7-074df07e0730>
2009-03-08 05:32:07 +0000

    fix new pywiki form
    git-svn-id: http://legobot.googlecode.com/svn@151 03ba9718-dac9-11dd-86d7-074df07e0730

--- trunk/toolserver/pui.py
+++ trunk/toolserver/pui.py
@@ -7,7 +7,8 @@
  
 import re, sys, os
 sys.path.append(os.environ['HOME'] + '/pywiki')
-import wiki
+from pywikibot import wiki
+wiki.setUser('Legobot')
 page = wiki.Page('Wikipedia:Possibly unfree images')
 wikitext = state0 = page.get()
 wikitext = re.compile(r'\n==New listings==', re.IGNORECASE).sub(r'\n*[[/{{subst:#time:Y F j|-14 days}}]]\n==New listings==', wikitext)
