89478bed86dfe4b522b2c38679410b4fb4b33979
legoktm.wikipedia <legoktm.wikipedia@03ba9718-dac9-11dd-86d7-074df07e0730>
2009-03-22 23:38:44 +0000

    update to pythonwikibot changes
    git-svn-id: http://legobot.googlecode.com/svn@165 03ba9718-dac9-11dd-86d7-074df07e0730

--- trunk/toolserver/fungi.py
+++ trunk/toolserver/fungi.py
@@ -1,18 +1,19 @@
 #!usr/bin/python
+#-*- coding:utf-8 -*-
 __version__ = '$Id$'
 import re, time, sys, os
 sys.path.append(os.environ['HOME'] + '/pywiki')
 from pywikibot import wiki, pagegen, timedate
 wiki.setUser('Legobot')
 
-def main():
-	gen = pagegen.transclude(wiki.Page('Template:WikiProject Fungi'))
+def do_project(page, putpage):
+	gen = pagegen.transclude(wiki.Page('Template' + page))
 	table = ''
 	for page in gen:
 		table += do_page(page)
-	putpage = wiki.Page('User:Legoktm/Bot')
+	putpage1 = wiki.Page(putpage)
 	newcontent = '{| class="wikitable sortable" style="text-align: left;"\n|-\n! Article name\n! Size (bytes)\n! Rating\n! Last modified\n! # Incoming links\n' + table + '\n|}'
-	putpage.put(newcontent, 'Testing bot')
+	putpage1.put(newcontent, 'Bot: Updating article list')
 def do_page(page):
 	talk_page = page
 	main_page = page.toggletalk()
@@ -28,8 +29,12 @@ def do_page(page):
 		clas = '{{FA-inline}}'
 	else:
 		clas = clas.title()
+	list = pagegen.whatlinkshere(main_page)
+	num=0
+	for i in list:
+		num+=1
 	try:
-		tablerow = '|-\n|| %s || %s || %s || %s || %s\n' %(main_page.aslink(), str(len(main_page.get())), clas, lastedit, str(len(pagegen.whatlinkshere(main_page))))
+		tablerow = '|-\n|| %s || %s || %s || %s || %s\n' %(main_page.aslink(), str(len(main_page.get())), clas, lastedit, str(num))
 	except wiki.IsRedirectPage:
 		return ''
 	except UnicodeEncodeError:
@@ -38,6 +43,9 @@ def do_page(page):
 		return ''
 	print tablerow,
 	return tablerow
+def main():
+	do_project('WikiProject Fungi','Wikipedia:WikiProject Fungi/fungus articles by size')
+	do_project('WikiProject Egypt','Wikipedia:WikiProject_Egypt/Articles_by_size')
 
 if __name__ == '__main__':
 	main()
