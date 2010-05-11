72091c398245ec6262f305d9e8d3b0eb93ff6a6c
legoktm <legoktm@03ba9718-dac9-11dd-86d7-074df07e0730>
2010-05-11 01:25:29 +0000

    line got randomly deleted
    git-svn-id: http://legobot.googlecode.com/svn@202 03ba9718-dac9-11dd-86d7-074df07e0730

--- trunk/toolserver/pui.py
+++ trunk/toolserver/pui.py
@@ -9,6 +9,7 @@ import re, sys, os
 import wikipedia as wiki
 site=wiki.getSite()
 page = wiki.Page(site,'Wikipedia:Possibly unfree files')
+wikitext = page.get()
 wikitext = re.compile(r'\n==New listings==', re.IGNORECASE).sub(r'\n*[[/{{subst:#time:Y F j|-14 days}}]]\n==New listings==', wikitext)
 EditMsg = 'Adding new day to holding cell'
 page.put(wikitext,EditMsg)
