27dce1aa0ef1bf0ce87148437972870f8aee5811
legoktm <legoktm@03ba9718-dac9-11dd-86d7-074df07e0730>
2010-05-11 01:22:24 +0000

    convert from pythonwikibot --> pywikipedia
    git-svn-id: http://legobot.googlecode.com/svn@201 03ba9718-dac9-11dd-86d7-074df07e0730

--- trunk/toolserver/pui.py
+++ trunk/toolserver/pui.py
@@ -2,21 +2,13 @@
 # -*- coding: utf-8  -*
  
 #
-# (C) Legoktm 2008-2009, MIT License
+# (C) Legoktm 2008-2010, MIT License
 # 
  
 import re, sys, os
-sys.path.append(os.environ['HOME'] + '/pythonwikibot')
-#sys.path.append('/Users/kman/projects/pywikibot')
-import wiki
-wiki.setUser('Legobot')
-page = wiki.Page('Wikipedia:Possibly unfree images')
-try:
-	wikitext = state0 = page.get()
-except wiki.IsRedirectPage:
-	page = wiki.Page('Wikipedia:Possibly unfree files')
-	wikitext = state0 = page.get()	
+import wikipedia as wiki
+site=wiki.getSite()
+page = wiki.Page(site,'Wikipedia:Possibly unfree files')
 wikitext = re.compile(r'\n==New listings==', re.IGNORECASE).sub(r'\n*[[/{{subst:#time:Y F j|-14 days}}]]\n==New listings==', wikitext)
 EditMsg = 'Adding new day to holding cell'
-wiki.showDiff(state0, wikitext)
-page.put(wikitext,EditMsg, bot=True)
+page.put(wikitext,EditMsg)
