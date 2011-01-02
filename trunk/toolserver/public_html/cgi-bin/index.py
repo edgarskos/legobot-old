baf18c097969a67d411c87b77cbe81f0a0814d65
legoktm <legoktm@03ba9718-dac9-11dd-86d7-074df07e0730>
2011-01-02 02:12:15 +0000

    update main page
    git-svn-id: http://legobot.googlecode.com/svn@216 03ba9718-dac9-11dd-86d7-074df07e0730

--- trunk/toolserver/public_html/cgi-bin/index.py
+++ trunk/toolserver/public_html/cgi-bin/index.py
@@ -10,17 +10,17 @@ content = """\
 <li><a href="http://en.wikipedia.org/wiki/User:Legobot">Legobot</a></li>
 <li><a href="http://en.wikipedia.org/wiki/User:Legobot II">Legobot II</a></li>
 <li><a href="http://en.wikipedia.org/wiki/User:Legobot III">Legobot III</a></li>
-<li><a href="http://en.wikipedia.org/wiki/User:ArticleAlertbot">ArticleAlertbot</a></li>
+<li><i><a href="http://en.wikipedia.org/wiki/User:ArticleAlertbot">ArticleAlertbot</a> Ð Inactive</i></li>
 </ul>
 <h1>Tools</h1>
 <ul>
 <li><a href="http://toolserver.org/~legoktm/cgi-bin/count.py">Raw edit counter</a></li>
 <li><a href="http://toolserver.org/~legoktm/alertbot.html">Alertbot status</a></li>
-</ul>
-<h2>Beta Tools</h2>
-<ul>
 <li><a href="http://toolserver.org/~legoktm/cgi-bin/reflinks.py">Template filler</a></li>
 </ul>
+<!--<h2>Beta Tools</h2>
+<ul>
+</ul>-->
 """
 page = monobook2.Page('~legoktm','/~legoktm/cgi-bin/index.py')
 print page.top()
